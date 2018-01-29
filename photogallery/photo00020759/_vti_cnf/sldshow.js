8fa22b19e9dab82bf7e5ee0136c5b72fcc3011e1
sasabrezo <sasabrezo@hotmail.com>
2018-01-29 22:05:58 +0100

    first commit

---
+++ photogallery/photo00020759/_vti_cnf/sldshow.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TW|28 Jan 2017 14:00:20 -0000
+vti_author:SR|BREZO-PC\\Brezo
+vti_modifiedby:SR|BREZO-PC\\Brezo
+vti_nexttolasttimemodified:TW|28 Jan 2017 14:00:20 -0000
+vti_timecreated:TR|28 Jan 2017 13:48:54 -0000
+vti_extenderversion:SR|6.0.2.5516
+vti_backlinkinfo:VX|photogallery/photo00020759/real.htm
+vti_cacheddtm:TX|28 Jan 2017 13:48:54 -0000
+vti_filesize:IR|4651
